04a5d8184aafb829ad1711f27be86a8fb7e0bc3a
mpetrovich <mark.petrovich@ramax.ru>
2023-05-26 12:46:49 +0300

    add config eslint and prettier

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import React from 'react';
-import {Book} from './Book';
+import React from "react";
+import { Book } from "./Book";
 
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
     <div>
-      <Book name='JS' year='2018' price='1000' />
-      <Book name='React' year='2020' price='5000' />
-      <Book name='Vue' year='2019' price='3000' />
+      <Book name="JS" year="2018" price="1000" />
+      <Book name="React" year="2020" price="5000" />
+      <Book name="Vue" year="2019" price="3000" />
     </div>
   );
 };
-
 
 export default App;
--- src/Book.jsx
+++ src/Book.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const Book = (props) => {
-    return (
-      <div>
-        <h2>Hello, {props.name ? props.name : 'default'}</h2>
-        <p>{props.year}</p>
-        <p>{props.price}</p>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h2>Hello, {props.name ? props.name : "default"}</h2>
+      <p>{props.year}</p>
+      <p>{props.price}</p>
+    </div>
+  );
+};
 
-  export { Book };
+export { Book };
