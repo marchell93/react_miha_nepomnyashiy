555ab227b3be87d8f1270cdb11a23642087d37d8
mpetrovich <mark.petrovich@ramax.ru>
2023-05-26 12:13:15 +0300

    Change 1.006

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Book} from './Book';
+
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <Book name='JS' year='2018' price='1000' />
+      <Book name='React' year='2020' price='5000' />
+      <Book name='Vue' year='2019' price='3000' />
+    </div>
+  );
+};
+
+
+export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,42 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-
-const Book = (props) => {
-  return (
-    <div>
-      <h2>Hello, {props.name}</h2>
-      <p>{props.year}</p>
-      <p>{props.price}</p>
-    </div>
-  );
-  // React.createElement('div', {}, [
-  //   React.createElement('h2', {}, props.name),
-  //   React.createElement('p', {}, props.year),
-  //   React.createElement('p', {}, props.price),
-  // ])
-}
-
-const App = () => {
-  return (
-  <div>
-    <Book name='JS' year='2018' price='1000' />
-    <Book name='React' year='2020' price='5000' />
-    <Book name='Vue' year='2019' price='3000' />
-  </div>
-  );
-  // 
-  // React.createElement('div', {}, [
-  //   React.createElement('h1', {id: 'hello', className: 'class1'}, 'Hello from react'),
-  //   React.createElement(Book),
-  //   React.createElement(Book),
-  //   React.createElement(Book),
-//   ])
-}
-
-// ReactDOM.render(React.createElement(App), document.getElementById('root'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
