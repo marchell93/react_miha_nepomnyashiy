e1586346f401eb8f8a3c2f5fa3f697747d25f77b
mpetrovich <mark.petrovich@ramax.ru>
2023-06-05 11:56:53 +0300

    add component instance

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ const App = () => {
       <Book name="JS" year="2018" price="1000" />
       <Book name="React" year="2020" price="5000" />
       <Book name="Vue" year="2019" price="3000" />
+      <Book name="Python" year="2015" price="5000"></Book>
     </div>
   );
 };
